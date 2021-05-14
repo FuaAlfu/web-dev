2b4fed438e695bcc67d8890fc3630ee56fc766ae
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-05-14 17:36:35 +0300

    add

---
+++ 000-pages-design/002-loading-spinner/app.js
@@ -0,0 +1,7 @@
+const date = () => {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    document.getElementById('d').innerHTML = currentYear.toString(); 
+}
+
+date();
