a001e6abeb92a86430eddfd0aea2e0e57fd4dc2f
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-07-01 00:05:28 +0300

    add

---
+++ 000-pages-design/010-scroll-up/app.js
@@ -0,0 +1,19 @@
+const span = document.querySelector('.up');
+
+window.onscroll = function() {
+    //console.log(this.scrollY); //for testing
+    // if(this.scrollY >= 1000)
+    // {
+    //     span.classList.add("show");
+    // }else{
+    //     span.classList.remove("show");
+    // }
+    this.scrollY >= 1000? span.classList.add("show") : span.classList.remove("show");
+};
+
+span.onclick = function() {
+    window.scrollTo({
+        top:0,
+        behavior: "smooth",
+    });
+};
